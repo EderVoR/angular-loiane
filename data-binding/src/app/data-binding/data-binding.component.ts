ed230e76bb555594f5c58430a78ddcf647ce53fe
Eder Vieira <eder.edy@gmail.com>
2022-08-25 16:48:18 -0400

    Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,7 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+    .highlight{
+      background-color: yellow;
+      font-weight: bold;
+    }
+  `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
@@ -11,12 +19,34 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com.br/500/400/?1';
 
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+
+  salvarValor(valor: any){
+    var elemento = valor as HTMLInputElement;
+    this.valorSalvo = elemento.value;
+  }
+
+  onMouseOverOur(){
+     this.isMouseOver = !this.isMouseOver;
+  }
+
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('Me feche');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    console.log();
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
   }
 
   constructor() { }
