d631e6338903698a4673bc39143017f156b646c5
Eder Vieira <eder.edy@gmail.com>
2022-10-27 17:10:27 -0400

    Escopo de instancia de serviço e modulos (singleton e várias instancias)

--- 05-servicos/src/app/cursos/cursos.service.ts
+++ 05-servicos/src/app/cursos/cursos.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class CursosService{
 
+	private cursos: string[] = ['Angular', '.Net', 'C#'];
+
+	constructor(){
+		console.log("Entrou aqui no service");
+	}
+
 	getCurso(){
-		return ['Angular', '.Net', 'C#'];
+		return this.cursos;
+	}
+
+	addCurso(curso: string){
+		this.cursos.push(curso);
 	}
 }
