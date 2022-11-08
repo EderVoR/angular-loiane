b1e6eae1da8f20c8fd8a7a5225b1bd73541577d0
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2022-11-08 14:43:08 -0400

    Difinindo e extraindo parametros de roteamento

--- 07-rotas/src/app/app.routing.ts
+++ 07-rotas/src/app/app.routing.ts
@@ -1,3 +1,4 @@
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { RouterModule } from "@angular/router";
 import { Routes } from '@angular/router';
 import { ModuleWithProviders } from "@angular/core";
@@ -8,8 +9,10 @@ import { HomeComponent } from './home/home.component';
 
 const APP_ROUTER: Routes = [
   {path: 'cursos', component: CursosComponent},
+  {path: 'curso/:id', component: CursoDetalheComponent},
   {path: 'login', component: LoginComponent},
   {path: '', component: HomeComponent}
 ]
 
 export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(APP_ROUTER);
+
