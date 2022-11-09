f1083ab76b5e2abf1c38570339221a8d44e64838
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2022-11-09 09:01:23 -0400

    Rotas imperativas, redirecionamento via codigo

--- 07-rotas/src/app/app.routing.ts
+++ 07-rotas/src/app/app.routing.ts
@@ -1,4 +1,3 @@
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { RouterModule } from "@angular/router";
 import { Routes } from '@angular/router';
 import { ModuleWithProviders } from "@angular/core";
@@ -6,11 +5,14 @@ import { ModuleWithProviders } from "@angular/core";
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 const APP_ROUTER: Routes = [
   {path: 'cursos', component: CursosComponent},
   {path: 'curso/:id', component: CursoDetalheComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
   {path: '', component: HomeComponent}
 ]
 
--- 07-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ 07-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
+import { CursosService } from './../cursos/cursos.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,8 +12,12 @@ export class CursoDetalheComponent implements OnInit {
 
   id: string = '';
   inscricao?: Subscription;
+  curso: any;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private cursosService: CursosService) {
     //this.id = this.route.snapshot.params['id'];
    }
 
@@ -20,6 +25,12 @@ export class CursoDetalheComponent implements OnInit {
       this.inscricao = this.route.params.subscribe(
         (params: any) => {
           this.id = params['id'];
+
+          this.curso = this.cursosService.getCurso(params['id']);
+
+          if(this.curso == null){
+            this.router.navigate(['naoEncontrado']);
+          }
         })
   }
 
