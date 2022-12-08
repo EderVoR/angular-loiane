8a3649e5fff2c9e5b566aaa5f172134320426654
Eder <eder.edy@gmail.com>
2022-12-08 13:52:00 -0400

    Refatoração de chamada de modulo

--- 07-rotas/src/app/cursos/cursos.routing.module.ts
+++ 07-rotas/src/app/cursos/cursos.routing.module.ts
@@ -1,7 +1,5 @@
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { Routes } from '@angular/router';
-import { ModuleWithProviders } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 import { CursosComponent } from './cursos.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
@@ -9,8 +7,8 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 const cursosRouter: Routes = [
   {path: '', component: CursosComponent},
-  {path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
   {path: ':id', component: CursoDetalheComponent},
+  {path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
 ]
 
 export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(cursosRouter);
